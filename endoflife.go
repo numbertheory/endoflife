b5db71e9cecc820728dd9a592ee580fa48ed96da
JP Etcheber <jetcheber@gmail.com>
2023-03-04 21:18:33 -0800

    show results for all product cycles

--- endoflife.go
+++ endoflife.go
@@ -7,15 +7,25 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 )
 
-type Product struct {
+type ProductCycle struct {
 	Eol               string `json:"eol"`
 	Latest            string `json:"latest"`
 	LatestReleaseDate string `json:"latestReleaseDate"`
 	ReleaseDate       string `json:"releaseDate"`
 	Lts               bool   `json:"lts"`
 }
+
+type Product struct {
+	Eol               []ProductCycle `json:"eol"`
+	Latest            []ProductCycle `json:"latest"`
+	LatestReleaseDate []ProductCycle `json:"latestReleaseDate"`
+	ReleaseDate       []ProductCycle `json:"releaseDate"`
+	Lts               []ProductCycle `json:"lts"`
+}
+
 
 const usage = `Usage of endoflife:
   -p, --product [PRODUCT]
@@ -27,6 +37,10 @@ const usage = `Usage of endoflife:
 
 func getProductCycle(productName string, cycle string) {
 	var url = "https://endoflife.date/api/" + productName + "/" + cycle + ".json"
+	if (cycle == "") {
+		url = "https://endoflife.date/api/" + productName + ".json"
+	}
+	
 	resp, err := http.Get(url)
 	if err != nil {
 		log.Fatalln(err)
@@ -35,9 +49,26 @@ func getProductCycle(productName string, cycle string) {
 	defer resp.Body.Close()
 	bodyBytes, _ := ioutil.ReadAll(resp.Body)
 
-	var product Product
-	json.Unmarshal(bodyBytes, &product)
-	fmt.Printf(product.Latest + "\n")
+	if (cycle == "") { 
+		product := make([]ProductCycle, 0)
+		json.Unmarshal([]byte(bodyBytes), &product)
+		fmt.Printf("|    EOL     |   Latest Version  |\n")
+		fmt.Printf("+============+===================+\n")
+		for i := 0; i < len(product); i++ {
+			buffer := strings.Repeat(" ", 18 - len(product[i].Latest))
+			fmt.Printf("| " + product[i].Eol + " | " + product[i].Latest + buffer + "|\n")
+		}
+		fmt.Printf("+============+===================+\n")
+	} else {
+		var productCycle ProductCycle
+		json.Unmarshal(bodyBytes, &productCycle)
+		fmt.Printf("|    EOL     |   Latest Version  |\n")
+		fmt.Printf("+============+===================+\n")
+		buffer := strings.Repeat(" ", 18 - len(productCycle.Latest))
+		fmt.Printf("| " + productCycle.Eol + " | " + productCycle.Latest + buffer + "|\n")
+		fmt.Printf("+============+===================+\n")
+	}
+
 }
 
 func main() {
@@ -51,6 +82,8 @@ func main() {
 	flag.Parse()
 	if product != "" && cycle != "" {
 		getProductCycle(product, cycle)	
+	} else if (product != "") {
+		getProductCycle(product, "")	
 	} else {
 		fmt.Print(usage)
 	}
